993979ba525f49a0c49a0caf5d50192555fca940
omkarbind <omkarbind132@gmail.com>
2026-04-13 20:13:56 +0530

    add readme file

--- app/src/main/java/com/om/notebook/ui/LoginScreen.kt
+++ app/src/main/java/com/om/notebook/ui/LoginScreen.kt
@@ -1,8 +1,5 @@
 package com.om.notebook.ui
 
-import android.R.attr.alpha
-import android.R.attr.translationY
-import android.widget.Toast
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
@@ -22,7 +19,6 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
